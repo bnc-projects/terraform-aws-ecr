fcd4fea4e19665bc6b43975dd79dd06842f519ff
Bhavik Kumar <bhavik@bravenewcoin.com>
2019-05-24 15:57:33 +1200

    Update to terraform 0.12 (#2)
    Update to terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,31 +1,33 @@
 variable "allowed_read_principals" {
   description = "allowed_read_principals defines which external principals are allowed to read from the ECR repository"
-  type        = "list"
+  type        = list
 }
 
 variable "allowed_write_principals" {
   description = "allowed_write_principals defines which external principals are allowed to write to the ECR repository"
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "ecr_repo_name" {
-  type        = "string"
+  type        = string
   description = "The name of the repository"
 }
 
 variable "enable_ecr_lifecycle" {
   description = "Set to false to prevent the module from creating a ECR lifecycle policy"
   default     = true
+  type        = bool
 }
 
 variable "max_images" {
   description = "The maximum number of images the repository should contain"
   default     = 100
+  type        = number
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   description = "A map of tags to add to all resources"
   default     = {}
 }
